9d16d7100719ce18a4f6fd27261d31bad26ee68e
Nguyen Tc <nguyentc21@gmail.com>
2023-09-28 14:19:44 +0700

    upgrade

--- src/components/ModalSection.tsx
+++ src/components/ModalSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 
 import ModalView from './ModalView';
@@ -10,101 +10,78 @@ import styles from '../styles';
 import type { StyleProp, ViewStyle } from 'react-native';
 import type { NestedModalProps } from '../types';
 
-const MainModal = memo<{
+const MainModal = (props: {
   visible?: boolean;
   wrapStyle?: StyleProp<ViewStyle>;
   defaultModalProps?: Partial<NestedModalProps>;
-}>(
-  (props) => {
-    const { visible = true, wrapStyle, defaultModalProps } = props;
-    const {
-      containerStyle: defaultContainerStyle,
-      wrapContainerStyle: defaultWrapContainerStyle,
-      backdropStyle: defaultBackdropStyle,
-      ..._defaultModalProps
-    } = defaultModalProps || {};
-    const [modalListState, setModalListState] = useState<NestedModalProps[]>(
-      []
-    );
+}) => {
+  const { visible = true, wrapStyle, defaultModalProps } = props;
+  const {
+    containerStyle: defaultContainerStyle,
+    wrapContainerStyle: defaultWrapContainerStyle,
+    backdropStyle: defaultBackdropStyle,
+    ..._defaultModalProps
+  } = defaultModalProps || {};
+  const [modalListState, setModalListState] = useState<NestedModalProps[]>([]);
 
-    // Did mount && will unmount
-    useEffect(() => {
-      EventRegister.on('open-nested-modal', (modalProps: NestedModalProps) => {
-        setModalListState((_state) => {
-          return _state.concat(modalProps);
-        });
+  useEffect(() => {
+    EventRegister.on('open-nested-modal', (modalProps: NestedModalProps) => {
+      setModalListState((_state) => {
+        return _state.concat(modalProps);
       });
-      EventRegister.on('close-nested-modal', (args: { id: string }) => {
-        setModalListState((_state) => {
-          return _state.map((el) => {
-            if (el.id === args.id) {
-              return { ...el, visible: false };
-            }
-            return el;
-          });
-        });
-      });
-      EventRegister.on(
-        'update-nested-modal',
-        (modalProps: NestedModalProps) => {
-          setModalListState((_state) => {
-            return _state.map((el) => {
-              if (el.id === modalProps.id) {
-                return modalProps;
-              }
-              return el;
-            });
-          });
-        }
-      );
-      return () => {
-        EventRegister.rm('open-nested-modal');
-        EventRegister.rm('close-nested-modal');
-        EventRegister.rm('update-nested-modal');
-      };
-    }, []);
-
-    const _onCloseModal = (modalProps: NestedModalProps) => () => {
-      modalProps.onClose?.();
-      setModalListState((_state) =>
-        _state.filter((item) => item.id !== modalProps.id)
-      );
-    };
-
-    if (!visible || modalListState == undefined || modalListState.length < 1)
-      return null;
-
-    return (
-      <View style={[styles.modalWrapContainer, wrapStyle]}>
-        {modalListState.map((item) => {
-          const {
-            id,
-            modalType,
-            containerStyle,
-            wrapContainerStyle,
-            backdropStyle,
-            ..._modalProps
-          } = item;
-          if (modalType === 'slide') {
-            return (
-              <SliderModalView
-                key={id}
-                containerStyle={[defaultContainerStyle, containerStyle]}
-                wrapContainerStyle={[
-                  defaultWrapContainerStyle,
-                  wrapContainerStyle,
-                ]}
-                backdropStyle={[defaultBackdropStyle, backdropStyle]}
-                {..._defaultModalProps}
-                {..._modalProps}
-                onClose={_onCloseModal(item)}
-              />
-            );
+    });
+    EventRegister.on('close-nested-modal', (args: { id: string }) => {
+      setModalListState((_state) => {
+        return _state.map((el) => {
+          if (el.id === args.id) {
+            return { ...el, visible: false };
           }
+          return el;
+        });
+      });
+    });
+    EventRegister.on('update-nested-modal', (modalProps: NestedModalProps) => {
+      setModalListState((_state) => {
+        return _state.map((el) => {
+          if (el.id === modalProps.id) {
+            return modalProps;
+          }
+          return el;
+        });
+      });
+    });
+    return () => {
+      EventRegister.rm('open-nested-modal');
+      EventRegister.rm('close-nested-modal');
+      EventRegister.rm('update-nested-modal');
+    };
+  }, []);
+
+  const _onCloseModal = (modalProps: NestedModalProps) => () => {
+    modalProps.onClose?.();
+    setModalListState((_state) =>
+      _state.filter((item) => item.id !== modalProps.id),
+    );
+  };
+
+  if (!visible || modalListState == undefined || modalListState.length < 1)
+    return null;
+
+  return (
+    <View style={[styles.modalWrapContainer, wrapStyle]}>
+      {modalListState.map((item) => {
+        const {
+          id,
+          modalType,
+          containerStyle,
+          wrapContainerStyle,
+          backdropStyle,
+          ..._modalProps
+        } = item;
+        if (modalType === 'slide') {
           return (
-            <ModalView
+            <SliderModalView
               key={id}
-              maxHeight={'80%'}
               containerStyle={[defaultContainerStyle, containerStyle]}
               wrapContainerStyle={[
                 defaultWrapContainerStyle,
@@ -116,14 +93,22 @@ const MainModal = memo<{
               onClose={_onCloseModal(item)}
             />
           );
-        })}
-      </View>
-    );
-  },
-  (pProps, nProps) => {
-    if (pProps.visible !== nProps.visible) return false;
-    return true;
-  }
-);
+        }
+        return (
+          <ModalView
+            key={id}
+            maxHeight={'80%'}
+            containerStyle={[defaultContainerStyle, containerStyle]}
+            wrapContainerStyle={[defaultWrapContainerStyle, wrapContainerStyle]}
+            backdropStyle={[defaultBackdropStyle, backdropStyle]}
+            {..._defaultModalProps}
+            {..._modalProps}
+            onClose={_onCloseModal(item)}
+          />
+        );
+      })}
+    </View>
+  );
+};
 
 export default MainModal;
