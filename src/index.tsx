9642940aed9e9c009012c7abdfafdce7a9d29082
Nguyen Tc <nguyentc21@gmail.com>
2024-01-03 16:21:01 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import Modal from './components/Modal';
 import ModalSection from './components/ModalSection';
 import NestedModal from './components/NestedModal';
 
+export { getAutoTransformWidth } from './functions';
+
 export type { NestedModalProps, ModalViewProps } from './types';
 export { NestedModal, Modal };
 export default ModalSection;
